fd64385a50e172ff4102164ca9c52aa5961399eb
Jean Carlos Martins da Silva <86752469+jeanmartins@users.noreply.github.com>
2021-08-23 17:54:32 -0300

    Exercicio-7 finalizado

---
+++ Exercicio-7/Exercicio-7/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Exercicio_7 {
+    class Program {
+        static void Main(string[] args) {
+            Retangulo rect = new Retangulo();
+            Console.WriteLine("Entre a largura e altura do retângulo: ");
+            rect.Largura = double.Parse(Console.ReadLine());
+            rect.Altura = double.Parse(Console.ReadLine());
+            Console.WriteLine(rect);
+        }
+    }
+}
---
+++ Exercicio-7/Exercicio-7/Retangulo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio_7 {
+    class Retangulo {
+        public double Largura;
+        public double Altura;
+
+        public double Area() {
+            return Largura * Altura;
+        }
+        public double Perimetro() {
+            return (Largura * 2) + (Altura * 2);
+        }
+        public double Diagonal() {
+            return Math.Sqrt(Math.Pow(Largura,2) + Math.Pow(Altura,2));
+        }
+        public override string ToString() {
+            return "Area = " + Area() + "\n" + "Perímetro = " + Perimetro() + "\n" + "Diagonal = " + Diagonal();
+        }
+    }
+}
