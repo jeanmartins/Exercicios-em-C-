65bf084e37d7192a7300baad6a7cdc717f79c8d9
Jean Carlos Martins da Silva <86752469+jeanmartins@users.noreply.github.com>
2021-08-23 17:42:58 -0300

    Exercicio-6 finalizado

--- Exercicio-6/Exercicio-6/Produto.cs
+++ Exercicio-6/Exercicio-6/Produto.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Globalization;
 
 namespace Exercicio_6 {
     class Produto {
@@ -10,6 +9,16 @@ namespace Exercicio_6 {
 
         public double ValorTotalEmEstoque() {
             return Preco * Quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade) {
+            Quantidade += quantidade;
+        }
+        public void RemoverProdutos(int quantidade) {
+            Quantidade -= quantidade;
+        }
+        public override string ToString() {
+            return Nome + ", $" + Preco.ToString("F2", CultureInfo.InvariantCulture) + ", " + Quantidade + " unidades, Total: $ " + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
         }
     }
 }
