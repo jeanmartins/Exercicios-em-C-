086c1f1d623b74bbd4eed7a924690371deab99a0
Jean Carlos Martins da Silva <86752469+jeanmartins@users.noreply.github.com>
2021-08-30 17:37:07 -0300

    Estudando sobre encapsulamento

--- Exercicio-6/Exercicio-6/Produto.cs
+++ Exercicio-6/Exercicio-6/Produto.cs
@@ -3,22 +3,47 @@ using System.Globalization;
 
 namespace Exercicio_6 {
     class Produto {
-        public string Nome;
-        public double Preco;
-        public int Quantidade;
+        private string _nome;
+        private double _preco;
+        private int _quantidade;
 
+        public Produto() {
+
+        }
+
+        public Produto(string nome, double preco, int quantidade) {
+            this._nome = nome;
+            this._preco = preco;
+            this._quantidade = quantidade;
+        }
+
+        public string GetNome() {
+            return this._nome;
+        }
+        public void SetNome(string nome) {
+            if (nome != null && nome.Length > 1) {
+                this._nome = nome;
+            }
+        }
+
+        public double GetPreco() {
+            return this._preco;
+        }
+        public int GetQuantidade() {
+            return this._quantidade;
+        }
         public double ValorTotalEmEstoque() {
-            return Preco * Quantidade;
+            return _preco * _quantidade;
         }
 
         public void AdicionarProdutos(int quantidade) {
-            Quantidade += quantidade;
+            _quantidade += quantidade;
         }
         public void RemoverProdutos(int quantidade) {
-            Quantidade -= quantidade;
+            _quantidade -= quantidade;
         }
         public override string ToString() {
-            return Nome + ", $" + Preco.ToString("F2", CultureInfo.InvariantCulture) + ", " + Quantidade + " unidades, Total: $ " + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+            return _nome + ", $" + _preco.ToString("F2", CultureInfo.InvariantCulture) + ", " + _quantidade + " unidades, Total: $ " + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
         }
     }
 }
--- Exercicio-6/Exercicio-6/Program.cs
+++ Exercicio-6/Exercicio-6/Program.cs
@@ -4,33 +4,13 @@ using System.Globalization;
 namespace Exercicio_6 {
     class Program {
         static void Main(string[] args) {
-            
-            Produto p = new Produto();
 
-           Console.WriteLine("Entre os dados do produto: ");
-            Console.Write("Nome: ");
-            p.Nome = Console.ReadLine();
-            Console.Write("Preço: ");
-            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            Console.Write("Quantidade no Estoque: ");
-            p.Quantidade = int.Parse(Console.ReadLine());
+            Produto p = new Produto("TV", 500.0, 10);
 
-            Console.WriteLine();
-            Console.WriteLine("Dados do produto: " + p);
-            
-            Console.WriteLine();
-            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
-            int qte = int.Parse(Console.ReadLine());
-            p.AdicionarProdutos(qte);
-
-            Console.WriteLine();
-            Console.WriteLine("Dados atualizados: " + p);
-
-            Console.Write("Digite o número de produtos a ser removido do estoque: ");
-            qte = int.Parse(Console.ReadLine());
-            p.RemoverProdutos(qte);
-            Console.WriteLine("Dados atualizados: " + p);
-            
+            p.SetNome("TV 4K");
+            Console.WriteLine(p.GetNome());
+            Console.WriteLine(p.GetPreco());
+            Console.WriteLine(p.GetQuantidade());
         }
     }
 }
