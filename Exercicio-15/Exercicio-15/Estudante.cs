9a3c205978435d0dd51a54a05b7f419fc6b5c413
Jean Carlos Martins da Silva <86752469+jeanmartins@users.noreply.github.com>
2021-09-06 13:22:10 -0300

    Adicionado exercicio 15

---
+++ Exercicio-15/Exercicio-15/Estudante.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio_15 {
+    class Estudante {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public Estudante(string nome, string email) {
+            Name = nome;
+            Email = email;
+        }
+        public override string ToString() {
+            return Name+", "+Email;
+        }
+    }
+}
---
+++ Exercicio-15/Exercicio-15/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Exercicio_15 {
+    class Program {
+        static void Main(string[] args) {
+            Estudante[] vect = new Estudante[10];
+
+            Console.Write("Quantos quartos serão alugados? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++) {
+                Console.WriteLine();
+                Console.WriteLine($"Aluguel #{i}");
+                Console.Write("Nome: ");
+                string name = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+                vect[quarto] = new Estudante(name, email);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Quartos ocupados: ");
+            for (int i = 0; i < vect.Length; i++) {
+                if (vect[i] != null)
+                    Console.WriteLine(i + ": " + vect[i]);
+            }
+        }
+    }
+}
