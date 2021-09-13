2e7fc90e0bd240d422dda13427901f9cf1fa359f
Jean Carlos Martins da Silva <86752469+jeanmartins@users.noreply.github.com>
2021-09-13 08:52:13 -0300

    Trabalhando com enumeracoes

---
+++ Exercicio-22/Exercicio-22/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio_22.Entities.Enums
+{
+    enum OrderStatus : int
+    {
+        PendingPayment,
+        Processing,
+        Shipped,
+        Delivered
+    }
+}
---
+++ Exercicio-22/Exercicio-22/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Exercicio_22.Entities;
+using Exercicio_22.Entities.Enums;
+namespace Exercicio_22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order item = new Order
+            {
+                Id = 3587,
+                Moment = DateTime.Now,
+                Status = OrderStatus.Processing
+            };
+            Console.WriteLine(item);
+            Console.WriteLine();
+
+            string txt = OrderStatus.Processing.ToString();
+            Console.WriteLine(txt);
+            Console.WriteLine();
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+            Console.WriteLine(os);
+            
+        }
+    }
+}
