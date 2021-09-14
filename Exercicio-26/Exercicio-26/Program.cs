7e234fd04a5bbaef1e7033c8af38cb02974b2d44
Jean Carlos Martins da Silva <86752469+jeanmartins@users.noreply.github.com>
2021-09-14 13:29:23 -0300

    Inheritance exercise

---
+++ Exercicio-26/Exercicio-26/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Exercicio_26.Entities;
+namespace Exercicio_26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessAccount account = new BusinessAccount(8010, "Bob Brown", 100.00, 500.00);
+
+            Console.WriteLine(account.Balance);
+            //account.Balance = 200;
+        }
+    }
+}
