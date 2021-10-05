6b53e3bee64a16e44f22f417c64af79ccaed2029
jeanmartins <jean.carlos.martins00@gmail.com>
2021-10-05 11:15:43 -0300

    FileStream and StreamReder

---
+++ Exercicio-33/Exercicio-33/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+namespace Exercicio_33 {
+    class Program {
+        static void Main(string[] args) {
+            string path = @"c:\temp\file1.txt";
+
+            StreamReader sr = null;
+            try {
+                sr = File.OpenText(path);
+                while (!sr.EndOfStream) {
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+                }               
+            }
+            catch (IOException e) {
+                Console.WriteLine("An error accurred");
+                Console.WriteLine(e.Message);
+            }
+            finally {
+                if (sr != null) sr.Close();
+              
+            }
+        }
+    }
+}
