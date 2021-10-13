d40b5ff593db854ffd44cedcb51ef5e1bc43df3a
jeanmmartins <jean.carlos.martins00@gmail.com>
2021-10-13 10:21:47 -0300

    using IComparable

---
+++ Exercicio-42/Exercicio-42/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Exercicio_42.Entities;
+namespace Exercicio_42 {
+    class Program {
+        static void Main(string[] args) {
+            string path = @"c:\pasta\file.csv";
+
+            try {
+                using (StreamReader sr = File.OpenText(path)) {
+                    List<Employee> list = new List<Employee>();
+                    while (!sr.EndOfStream) {
+                        list.Add(new Employee(sr.ReadLine()));
+                    }
+                    list.Sort();
+                    foreach(Employee emp in list) {
+                        Console.WriteLine(emp);
+                    }
+                }
+            }
+            
+            catch (IOException e) {
+                Console.WriteLine("An error occurred: " + e.Message);
+            }
+            
+        }
+    }
+}
