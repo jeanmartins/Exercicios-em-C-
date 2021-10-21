aec78c4c59016d4e9649a60a476d4e27b53ea7b9
jeanmartins <jean.carlos.martins00@gmail.com>
2021-10-21 09:42:36 -0300

    exercise with hashset(unionwith)

---
+++ Exercicio-47/Exercicio-47/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_47
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> courseA = new HashSet<int>();
+            HashSet<int> courseB = new HashSet<int>();
+            HashSet<int> courseC = new HashSet<int>();
+            Console.Write("How many students for course A? ");
+            int n = int.Parse(Console.ReadLine());
+            for(int i =1; i<=n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                courseA.Add(x);
+            }
+            Console.Write("How many students for course B? ");
+            n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                courseB.Add(x);
+            }
+            Console.Write("How many students for course C? ");
+            n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                courseC.Add(x);
+            }
+            HashSet<int> allCourses = new HashSet<int>(courseA);
+            allCourses.UnionWith(courseB);
+            allCourses.UnionWith(courseC);
+
+            Console.WriteLine("Total students: " + allCourses.Count);
+        }
+    }
+}
