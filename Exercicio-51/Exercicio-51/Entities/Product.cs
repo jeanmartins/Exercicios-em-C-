db420366fca6b519221bf00731c20681b6d6fadd
jeanmartins <jean.carlos.martins00@gmail.com>
2021-10-28 08:41:31 -0300

    working with delegates, comparison and lambda

---
+++ Exercicio-51/Exercicio-51/Entities/Product.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+
+namespace Exercicio_51.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public override string ToString()
+        {
+            return Name + ", " + Price.ToString("F2", CultureInfo.InstalledUICulture);
+        }
+
+
+    }
+}
---
+++ Exercicio-51/Exercicio-51/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Exercicio_51.Entities;
+
+namespace Exercicio_51
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+            list.Add(new Product
+            {
+                Name = "TV",
+                Price = 4500.00
+            });
+
+            list.Add(new Product
+            {
+                Name = "Tablet",
+                Price = 300.00
+            });
+
+            list.Add(new Product
+            {
+                Name = "Notebook",
+                Price = 950.00
+            });
+
+
+            list.Sort((p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper()));
+
+            foreach(Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+        }
+
+    }
+}
