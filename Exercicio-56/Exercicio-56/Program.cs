e37e0a987d57879338dc42148a6599af3c4e01a7
jeanmartins <jean.carlos.martins00@gmail.com>
2021-11-01 12:29:18 -0300

    working with delegate Func, example with select

---
+++ Exercicio-56/Exercicio-56/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Exercicio_56.Entities;
+using System.Linq;
+
+namespace Exercicio_56
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+            Func<Product, string> func = NameUpper;
+            List<string> result = list.Select(func).ToList();
+            foreach (string s in result)
+            {
+                Console.WriteLine(s);
+            }
+        }
+
+        static string NameUpper(Product p)
+        {
+            return p.Name.ToUpper();
+        }
+    }
+}
