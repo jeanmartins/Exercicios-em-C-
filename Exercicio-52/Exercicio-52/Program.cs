90c0ecee997b981e534194ccd227199c301e6d20
jeanmartins <jean.carlos.martins00@gmail.com>
2021-11-01 11:39:57 -0300

    working with delegates

---
+++ Exercicio-52/Exercicio-52/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Exercicio_52.Services;
+namespace Exercicio_52
+{
+    delegate double BinaryNumericOperation(double n1, double n2);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10.0;
+            double b = 12.0;
+
+            BinaryNumericOperation op = CalculationService.Max;
+            double result = op.Invoke(a, b);
+            Console.WriteLine(result);
+        }
+    }
+}
