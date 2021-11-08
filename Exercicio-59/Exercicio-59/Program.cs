2b16133c1aa701b9653848ca3f46119d766a2b5a
jeanmartins <jean.carlos.martins00@gmail.com>
2021-11-08 12:16:43 -0300

    exercise with linq

--- Exercicio-59/Exercicio-59/Program.cs
+++ Exercicio-59/Exercicio-59/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.IO;
+using Exercicio_59.Entities;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 
 namespace Exercicio_59
 {
@@ -6,7 +11,33 @@ namespace Exercicio_59
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string path = @"/home/jean/Documentos/in";
+            List<Product> list = new List<Product>();
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string [] fields = sr.ReadLine().Split(',');
+                        string name = fields[0];
+                        double price = double.Parse(fields[1], CultureInfo.InvariantCulture);
+                        list.Add(new Product(name, price));
+                    }
+                }
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            var avg = list.Select(p => p.Price).DefaultIfEmpty().Average();
+            Console.WriteLine("Avarage price = " + avg.ToString("F2", CultureInfo.InvariantCulture));
+            var names = list.Where(p => p.Price < avg).OrderByDescending(p => p.Name).Select(p => p.Name);
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
         }
     }
 }
